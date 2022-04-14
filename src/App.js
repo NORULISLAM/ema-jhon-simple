cbf93b789cdf454684a7044c6d103b4f18a513eb
user <juyelr16@gmail.com>
2022-04-14 14:58:33 +0900

    implement login and signin

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from './component/Header/Header';
 import Inventory from './component/Inventory/Inventory';
 import Orders from './component/Orders/Orders';
 import Shop from './component/Shop/Shop';
+import SingUP from './component/SingUp/SingUp';
+import Login from './component/Login/Login';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path='/orders' element={<Orders></Orders>}></Route>
         <Route path='/inventory' element={<Inventory></Inventory>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/singup' element={<SingUP></SingUP>}></Route>
       </Routes>
     </div>
   );
---
+++ src/component/SingUp/SingUp.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+
+
+const SingUP = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setconfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth)
+
+
+    if (user) {
+        navigate('/shop');
+    }
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+    const handleConfirmPasswordBlur = event => {
+        setconfirmPassword(event.target.value);
+    }
+    const handleCreateUser = event => {
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            setError('Your two passwords did not match');
+            return;
+        }
+        if (password.length < 6) {
+            setPassword('password two must be 6 chartcter')
+        }
+        createUserWithEmailAndPassword(email, password);
+    }
+    return (
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Sign Up</h2>
+                <form onSubmit={handleCreateUser}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name='email' id='' />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name='password' id='' required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input onBlur={handleConfirmPasswordBlur} type="password" name='confirm-password' id='' required />
+                    </div>
+                    <p style={{ color: 'red' }}>
+                        {error}
+                    </p>
+                    <input className='form-submit' type="submit" value="Sign Up" required />
+                    <p>
+                        Already an account? <Link className='form-link' to="/login">Login</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default SingUP;
